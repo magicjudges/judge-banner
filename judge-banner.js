ff8c61fbeaf2497b5705911fa6c9b2790ab5548e
Joel <joel.krebs@sv.cmu.edu>
2015-02-05 17:03:01 -0800

    initial commit

---
+++ judge-banner.js
@@ -0,0 +1,24 @@
+jQuery.get('http://assets.aleaiactaest.ch/judge-banner/judge-banner.html', function (data) {
+	jQuery().ready(function () {
+
+		jQuery('body').prepend(data);
+		var hostname = window.location.hostname;
+		var elem;
+		if (hostname.indexOf('apps') != -1) {
+			elem = jQuery('#nav-apps');
+		} else if (hostname.indexOf('wiki') != -1) {
+			elem = jQuery('#nav-wiki');
+		} else if (hostname.indexOf('blogs') != -1) {
+			jQuery('#nav-blogs').addClass('current');
+		} else if (hostname.indexOf('chat') != -1) {
+			elem = jQuery('#nav-irc');
+		}
+
+		if (elem) {
+			elem.addClass('current');
+			text = elem.text();
+			elem.find('a').remove();
+			elem.html(text);
+		}
+	});
+});
